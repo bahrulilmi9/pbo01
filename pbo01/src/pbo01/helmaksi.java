c8f2a2fb3a4d13bc97c5447eeb97a43f019e8fda
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-03-27 19:58:05 +0800

    Membuat Class Aksi dan Method Main

---
+++ pbo01/src/pbo01/helmaksi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo01;
+
+/**
+ *
+ * @author ACER ASPIRE E5-475
+ */
+public class helmaksi {
+    public static void main(String[] args) {
+        
+    }
+}
