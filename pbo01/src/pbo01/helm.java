0469eae94c27dd7935546a18efa44a2cf57234a4
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-03-27 19:55:22 +0800

    Membuat Atribut dan Behavior

--- pbo01/src/pbo01/helm.java
+++ pbo01/src/pbo01/helm.java
@@ -10,5 +10,17 @@ package pbo01;
  * @author ACER ASPIRE E5-475
  */
 public class helm {
+    String jenis;
+    String warna;
+    String ukuran;
+    String merk;
+    String kaca;
     
+    void Pakaihelm(){
+        System.out.println("Jenis :"+jenis);
+        System.out.println("Warna :"+warna);
+        System.out.println("Ukuran :"+ukuran);
+        System.out.println("Merk :"+merk);
+        System.out.println("Kaca :"+kaca+"\n");
+    } 
 }
